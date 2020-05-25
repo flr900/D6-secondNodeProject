16fb1018b744f4dbe1a7b94fa7eda188d8ba8dac
Felipe Rodrigues <feliperodrigues9000@gmail.com>
2020-05-25 20:55:18 -0300

    KickOff2.0

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res:Response) => {
   return res.json({ message: 'Alo mundo' });
 });
 
