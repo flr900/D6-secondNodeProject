4e24a2e9716b6cffac252afa1889eb5c958b4294
Douglas Thomaz <douglas.thomaz@spot-edu.com.br>
2020-05-28 20:42:08 -0300

    RO-00 fixing build

--- dist/server.js
+++ dist/server.js
@@ -3,11 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+require("dotenv/config");
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+require("express-async-errors");
+var routes_1 = __importDefault(require("./routes"));
+require("./database");
 var app = express_1.default();
-app.get('/', function (req, res) {
-    return res.json({ message: 'Alo mundo' });
-});
-app.listen(3333, function () {
+app.use(cors_1.default());
+app.use(express_1.default.json());
+app.use(routes_1.default);
+var port = process.env.PORT || 3333;
+app.listen(port, function () {
     console.log('🚀 App launched');
 });
